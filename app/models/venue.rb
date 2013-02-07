ae6d8cdabc10b7328e7dc56f799cbd0917515ba8
David Anderson <daveande@gmail.com>
2013-02-07 16:48:16 -0500

    create formatted_address for venue

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,4 +1,12 @@
 class Venue < ActiveRecord::Base
   attr_accessible :address, :address2, :city, :name, :phone_number, :state, :user_id, :zipcode
   validates_presence_of :name, :address, :city, :state, :zipcode, :phone_number, :user_id
+
+  def formatted_address
+    if self.address2
+      "#{self.address}, " + "#{self.address2}"
+    else
+      self.address
+    end
+  end
 end
